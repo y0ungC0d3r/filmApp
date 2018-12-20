9cf2daf073ec9ff15fc390e9eb7bdcd5c27f1e4f
Wojtek <wowo098@gmail.com>
2018-12-20 14:45:40 +0100

    change country structure and data

--- src/main/java/org/student/filmApp/entity/Country_.java
+++ src/main/java/org/student/filmApp/entity/Country_.java
@@ -4,7 +4,7 @@ import javax.persistence.metamodel.SetAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 
 public class Country_ {
-    public static volatile SingularAttribute<Country, String> id;
+    public static volatile SingularAttribute<Country, Long> id;
     public static volatile SingularAttribute<Country, String> name;
     public static volatile SetAttribute<Country, Film> films;
 }
